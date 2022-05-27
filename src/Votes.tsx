737fd282ac54e0a12db705f53121dcc1e2562972
Amber <amber.klandrud@gmail.com>
2022-05-27 12:07:52 -0400

    Part one

---
+++ src/Votes.tsx
@@ -0,0 +1,14 @@
+export default function Votes () {
+    return (
+        <div>
+            <h1> Vote Here</h1>
+            <button>Chocolate</button>
+            <button>Vanilla</button>
+            <button>Strawberry</button>
+
+            <div className = "Chocolate"><strong>Chocolate</strong></div>
+            <div className = "Vanilla"><strong>Vanilla</strong></div>
+            <div className = "Strawberry"><strong>Strawberry</strong></div>
+        </div>
+    )
+}
